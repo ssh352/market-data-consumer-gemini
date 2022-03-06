75e4073a97dde2976c43d8925245d7a4b2f3b09a
Stephen Nimmo <stephennimmo@gmail.com>
2022-03-06 07:57:13 -0600

    Project init

---
+++ src/main/java/com/redhat/trm/marketdata/GreetingConfig.java
@@ -0,0 +1,12 @@
+package com.redhat.trm.marketdata;
+
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithName;
+
+@ConfigMapping(prefix = "greeting")
+public interface GreetingConfig {
+
+    @WithName("message")
+    String message();
+
+}
---
+++ src/test/java/com/redhat/trm/marketdata/NativeSubscriptionResourceIT.java
@@ -0,0 +1,9 @@
+package com.redhat.trm.marketdata;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeSubscriptionResourceIT extends SubscriptionResourceTest {
+
+    // Execute the same tests but in native mode.
+}
